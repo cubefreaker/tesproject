872bbb18a88119db1e60a4f9fc00dd97092dfccf
cubefreaker <hamzah_habibi@rocketmail.com>
2019-05-20 09:54:13 +0700

    update nama tabel yg belum sesuai

--- application/modules/member/controllers/Member.php
+++ application/modules/member/controllers/Member.php
@@ -530,7 +530,7 @@ class Member extends CI_Controller
             $contactData['user_id'] = $user->id;
             $this->load->model('m_insert');
             $dataInsert = [
-                'table' => 'users_kontak_perwakilan',
+                'table' => 'users_contact',
                 'data' => $contactData
             ];
             $this->m_insert->insertDynamic($dataInsert);
@@ -538,7 +538,7 @@ class Member extends CI_Controller
             $this->load->model('m_update');
             $dataUpdate = [
                 'data'  => $contactData,
-                'table' => 'users_kontak_perwakilan',
+                'table' => 'users_contact',
                 'where' => ['user_id' => $user->id]
             ];
             
